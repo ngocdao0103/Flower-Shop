9655edd36b4ed8e4ca09270a2f803c4a13d99519
quoc toan <nguyenthimysuong99999@gmail.com>
2025-11-04 10:03:41 +0700

    [JS_Admin]_CRUD Danh mục

--- config/api-endpoint.config.js
+++ config/api-endpoint.config.js
@@ -1,14 +1,20 @@
-export const endpoints = {
-    ORDER: "/orders",
-};
+export const endpoint = {
+    CATEGORY : '/categories',
+    PRODUCT : '/products',
+    ORDER : '/orders',
+    USER : '/users',
+    CART: '/carts',
+}
 
 export const status = {
-    OK: 200,
-    CREATED: 201,
-    NO_CONTENT: 204,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    FORBIDDEN: 403,
-    NOT_FOUND: 404,
-    INTERNAL_SERVER_ERROR: 500
+    OK : 200,
+    CREATED : 201,
+    DELETED : 204,
+    NOT_FOUND : 404,
+    SERVER_ERROR : 500,
+    BAD_REQUEST : 400,
+    UNAUTHORIZED : 401,
+    FORBIDDEN : 403,
+    CONFLICT : 409,
+    UNPROCESSABLE_ENTITY : 422,
 }
